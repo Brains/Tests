7713ef526725059951619411d08484b9840c01ce
CompleteBrains <completebrains@gmail.com>
2014-04-11 15:12:18 +0300

    Help button

--- Android/Activity1.cs
+++ Android/Activity1.cs
@@ -15,7 +15,7 @@ namespace AndroidApplication6
     [Activity (Label = "AndroidApplication6", MainLauncher = true, Icon = "@drawable/icon")]
     public class Activity1 : Activity
     {
-        int count = 1;
+        private int count = 1;
 
         protected override void OnCreate (Bundle bundle)
         {
@@ -25,16 +25,50 @@ namespace AndroidApplication6
 //            SetContentView (Resource.Layout.Main);
 //            SetContentView (new MyOvalShape (this));
 
+            FrameLayout.LayoutParams layoutParams;
+            layoutParams = new FrameLayout.LayoutParams (200, ViewGroup.LayoutParams.WrapContent);
+            SetContentView (CreateHelp (), layoutParams);
 
             // Get our button from the layout resource,
             // and attach an event to it
 //            Button button = FindViewById<Button> (Resource.Id.MyButton);
 //            button.Click += delegate { button.Text = string.Format ("{0} clicks!", count++); };
 
-            var viewGroup = new LinearLayout (ApplicationContext);
-            AddContentView (viewGroup, Window.Attributes);
-            viewGroup.AddView (new MyOvalShape (ApplicationContext));
+//            var viewGroup = new LinearLayout (ApplicationContext);
+//            AddContentView (viewGroup, Window.Attributes);
+//            viewGroup.AddView (new MyOvalShape (ApplicationContext));
+        }
 
+        private View CreateHelp()
+        {
+            var help = new LinearLayout (ApplicationContext);
+//            help.SetMinimumWidth (200);
+//            help.SetGravity (GravityFlags.Left);
+//            help.ShowDividers = ShowDividers.Beginning;
+//            help.SetForegroundGravity (GravityFlags.Right);
+//            help.LayoutParameters = new LinearLayout.LayoutParams (200, ViewGroup.LayoutParams.WrapContent); ;
+
+            LinearLayout.LayoutParams layoutParams;
+            layoutParams = new LinearLayout.LayoutParams (ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
+
+            
+
+            ImageView HelpView = new ImageView (ApplicationContext);
+            HelpView.SetImageResource (Android.Resource.Drawable.IcMenuHelp);
+            layoutParams.Gravity = GravityFlags.CenterVertical;
+//            help.AddView (HelpView, layoutParams);
+
+            TextView TextView = new TextView (ApplicationContext);
+            TextView.Text = "Shitty Shitty Shitty Shitty Shitty";
+            TextView.TextSize = 20;
+
+            var question = ApplicationContext.Resources.GetDrawable (Android.Resource.Drawable.IcMenuHelp);
+            TextView.SetPadding (question.IntrinsicWidth, 0, 0, 0);
+
+            help.AddView (TextView, layoutParams);
+
+
+            return help;
         }
     }
 
@@ -46,12 +80,12 @@ namespace AndroidApplication6
         public MyOvalShape (Context context)
             : base (context)
         {
-            var paint = new Paint ();
+            var paint = new Paint();
             paint.SetARGB (255, 200, 255, 0);
             paint.SetStyle (Android.Graphics.Paint.Style.Stroke);
             paint.StrokeWidth = 1;
 
-            _shape = new ShapeDrawable (new OvalShape ());
+            _shape = new ShapeDrawable (new OvalShape());
             _shape.Paint.Set (paint);
 
             _shape.SetBounds (0, 0, 300, 300);
@@ -74,8 +108,6 @@ namespace AndroidApplication6
             Paint.Color = Color.Chocolate;
             Paint.TextSize = 32;
             canvas.DrawText ("?", Width / 2.0f, Height / 2.0f, Paint);
-
         }
     }
 }
-

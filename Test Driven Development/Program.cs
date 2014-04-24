3fc23515db79319d8828ad4d8021b7b331ffb2b3
CompleteBrains <completebrains@gmail.com>
2014-04-24 08:56:54 +0300

    Tests

---
+++ Test Driven Development/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Driven_Development
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+        }
+
+        [TestMethod]
+        public void BasicRooterTest ()
+        {
+            // Create an instance to test:
+            Rooter rooter = new Rooter ();
+            // Define a test input and output value:
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+            // Run the method under test:
+            double actualResult = rooter.SquareRoot (input);
+            // Verify the result:
+            Assert.AreEqual (expectedResult, actualResult,
+                delta: expectedResult / 100);
+        }
+    }
+}

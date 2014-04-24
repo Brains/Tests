6ef86ed749d39fe10c06cd7795c7d5aed95d4b28
CompleteBrains <completebrains@gmail.com>
2014-04-24 08:57:12 +0300

    Update: Solution

--- Android/Activity.cs
+++ Android/Activity.cs
@@ -22,54 +22,46 @@ namespace Test
             base.OnCreate (bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+//            SetContentView (Resource.Layout.Main);
             SetContentView (new TestView (this));
 
-//             Get our button from the layout resource,
-//             and attach an event to it
-            Button button = FindViewById<Button> (Resource.Id.MyButton);
-            button.Click += delegate { button.Text = string.Format ("{0} clicks!", count++); };
-
-            var viewGroup = new LinearLayout (ApplicationContext);
-            AddContentView (viewGroup, Window.Attributes);
-            viewGroup.AddView (new TestView (ApplicationContext));
+            // Get our button from the layout resource, and attach an event to it
+//            Button button = FindViewById<Button> (Resource.Id.MyButton);
+//            button.Click += delegate { button.Text = string.Format ("{0} clicks!", count++); };
+//
+//            var viewGroup = new LinearLayout (ApplicationContext);
+//            AddContentView (viewGroup, Window.Attributes);
+//            viewGroup.AddView (new TestView (ApplicationContext));
         }
-
     }
 
     //------------------------------------------------------------------
     public class TestView : TextView
     {
-        private readonly ShapeDrawable _shape;
         private Bitmap bitmap;
+        private Canvas canvas;
 
+        //------------------------------------------------------------------
         public TestView (Context context) : base (context)
         {
-            SetSoftwareLayer();
-
-            bitmap = Bitmap.CreateBitmap (300, 300, Android.Graphics.Bitmap.Config.Argb8888);
+            bitmap = Bitmap.CreateBitmap (400, 400, Android.Graphics.Bitmap.Config.Argb8888);
             bitmap.EraseColor (Color.Orange);
+
+            canvas = new Canvas (bitmap);
         }
 
         //------------------------------------------------------------------
-        private void SetSoftwareLayer ()
+        protected override void OnDraw (Canvas canvas)
         {
-            SetLayerType (Android.Views.LayerType.Hardware, null);
-            Console.WriteLine(LayerType);
+            Rect rect = new Rect (0, 0, 1000, 1000);
+//            canvas.DrawBitmap (bitmap, null, rect, Paint);
+            canvas.DrawBitmap (bitmap, 0, 0, Paint);
 
+//            canvas.Translate (500, 500);
+//            bitmap.EraseColor (Color.GreenYellow);
+//            canvas.DrawBitmap (bitmap, 0, 0, Paint);
 
-            const int layerTypeSoftware = 1;
-
-            if ((int) Build.VERSION.SdkInt >= 11)
-            {
-                var method = JNIEnv.GetMethodID (JNIEnv.GetObjectClass (Handle), "setLayerType", "(ILandroid/graphics/Paint;)V");
-                JNIEnv.CallVoidMethod (Handle, method, new JValue (layerTypeSoftware), new JValue (null));
-            }
-
-
-            Console.WriteLine (LayerType);
-
-
+            base.OnDraw (canvas);
         }
     }
 }

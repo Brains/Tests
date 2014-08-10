982331462c8289a1a81adbdcf7c59ca83551c109
CompleteBrains <completebrains@gmail.com>
2014-08-10 17:32:26 +0300

    WPF: Button's hover animation

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,35 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow ()
+        public MainWindow()
         {
-            InitializeComponent ();
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter (object sender, MouseEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation (1, 1.2, new Duration (TimeSpan.FromSeconds (0.1)));
+
+            ScaleTransform transform = new ScaleTransform ();
+            Button.RenderTransform = transform;
+            transform.CenterX = Button.ActualWidth / 2;
+            transform.CenterY = Button.ActualHeight / 2;
+
+            transform.BeginAnimation (ScaleTransform.ScaleXProperty, animation);
+            transform.BeginAnimation (ScaleTransform.ScaleYProperty, animation);
+        }
+
+        private void Button_MouseLeave (object sender, MouseEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation (1.2, 1, new Duration (TimeSpan.FromSeconds (0.1)));
+
+            ScaleTransform transform = new ScaleTransform ();
+            Button.RenderTransform = transform;
+            transform.CenterX = Button.ActualWidth / 2;
+            transform.CenterY = Button.ActualHeight / 2;
+
+            transform.BeginAnimation (ScaleTransform.ScaleXProperty, animation);
+            transform.BeginAnimation (ScaleTransform.ScaleYProperty, animation);
         }
     }
 }

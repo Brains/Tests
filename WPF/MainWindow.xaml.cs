feed882e62fb4a7f072ba1db612b23fb7ce71fd9
Brains <CompleteBrains@gmail.com>
2014-12-11 19:47:33 +0200

    Filter data in Grid by Name Sorting by Color

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,55 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    //-------------------------------------------------------------------
+    public class DataEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+
+    //-------------------------------------------------------------------
     public partial class MainWindow : Window
     {
+        public DataEntry[] Data;
+
+        //-------------------------------------------------------------------
         public MainWindow()
         {
             InitializeComponent();
+            Grid.ColumnWidth = 100;
+
+            Loaded += OnLoaded;
         }
 
-        private void Button_MouseEnter (object sender, MouseEventArgs e)
+        //-------------------------------------------------------------------
+        void OnLoaded(object sender, RoutedEventArgs e)
         {
-            DoubleAnimation animation = new DoubleAnimation (1, 1.2, new Duration (TimeSpan.FromSeconds (0.1)));
-
-            ScaleTransform transform = new ScaleTransform ();
-            Button.RenderTransform = transform;
-            transform.CenterX = Button.ActualWidth / 2;
-            transform.CenterY = Button.ActualHeight / 2;
-
-            transform.BeginAnimation (ScaleTransform.ScaleXProperty, animation);
-            transform.BeginAnimation (ScaleTransform.ScaleYProperty, animation);
+            FillGrid();
+            FillComboBox();
         }
 
-        private void Button_MouseLeave (object sender, MouseEventArgs e)
+        //-------------------------------------------------------------------
+        private void FillComboBox()
         {
-            DoubleAnimation animation = new DoubleAnimation (1.2, 1, new Duration (TimeSpan.FromSeconds (0.1)));
+            SortedSet<string> set = new SortedSet<string>();
 
-            ScaleTransform transform = new ScaleTransform ();
-            Button.RenderTransform = transform;
-            transform.CenterX = Button.ActualWidth / 2;
-            transform.CenterY = Button.ActualHeight / 2;
+            // Get list of unique names
+            foreach (var entry in Data)
+                set.Add(entry.Name);
 
-            transform.BeginAnimation (ScaleTransform.ScaleXProperty, animation);
-            transform.BeginAnimation (ScaleTransform.ScaleYProperty, animation);
+            // Put them all into ComboBox
+            foreach (var entry in set)
+                Name.Items.Add(entry);
         }
+
+        //-------------------------------------------------------------------
+        public void FillGrid()
+        {
+            Data = new DataEntry[]
+            {
+                new DataEntry {Id = 1, Name = "Johny", Color = "White"},
+                new DataEntry {Id = 2, Name = "Merry", Color = "Red"},
+                new DataEntry {Id = 3, Name = "Sindi", Color = "Green"},
+                new DataEntry {Id = 4, Name = "Johny", Color = "Red"},
+                new DataEntry {Id = 5, Name = "Morrow", Color = "Yellow"},
+                new DataEntry {Id = 6, Name = "Johny", Color = "Red"},
+                new DataEntry {Id = 7, Name = "Sindi", Color = "Blue"}
+            };
+
+            this.Grid.ItemsSource = Data;
+        }
+
+        //-------------------------------------------------------------------
+        private void OnComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Data == null) return;
+
+            var filter = Name.SelectedValue;
+
+            // Show all entries in Grid
+            if (filter.ToString() == "All")
+            {
+                Grid.ItemsSource = Data;
+                return;
+            }
+
+            // Or filter entries by selection in the ComboBox
+            Grid.ItemsSource = from entry in Data
+                               where entry.Name == filter.ToString()
+                               select entry;
+        }
+
+        //-------------------------------------------------------------------
+        private void OnSortButtonClick(object sender, RoutedEventArgs e)
+        {
+            Grid.ItemsSource = from entry in Data
+                               orderby entry.Color
+                               select entry;
+
+        }
+
+
     }
 }

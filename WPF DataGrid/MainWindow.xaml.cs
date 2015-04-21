7c9a95ac20deaf35e6193c9d59f016dae1851eb6
Brains <CompleteBrains@gmail.com>
2015-04-21 13:12:02 +0300

    Move project

---
+++ WPF DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WPF
+{
+    //-------------------------------------------------------------------
+    public partial class MainWindow : Window
+    {
+        //-------------------------------------------------------------------
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var viewModel = new WindowViewModel();
+            
+            DataContext = viewModel;
+        }
+
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine ("Break");
+	        TestTextBox.Text = "Clicked";
+        }
+    }
+}
--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,31 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace WPF
 {
-    //-------------------------------------------------------------------
-    public partial class MainWindow : Window
-    {
-        //-------------------------------------------------------------------
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            var viewModel = new WindowViewModel();
-            
-            DataContext = viewModel;
-        }
-
-        private void Button_Click (object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine ("Break");
-	        TestTextBox.Text = "Clicked";
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow ()
+		{
+			InitializeComponent();
+		}
+	}
 }
